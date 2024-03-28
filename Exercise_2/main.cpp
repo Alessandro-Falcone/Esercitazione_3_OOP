2fef81951b22eaa5f30ea513dc039cdd8ae8dccc
alexf <alexf@LAPTOP-L8J25IH7>
2024-03-28 11:08:04 +0100

    My second commit

--- Exercise_2/main.cpp
+++ Exercise_2/main.cpp
@@ -4,17 +4,28 @@
 
 using namespace std;
 
+void trasformazioneConiugato(float& parteRea, float& parteImm){
+    parteRea = parteRea;
+    parteImm = -parteImm;
+}
+
 int main()
 {
     float sumReale = 0;
     float sumImmaginaria = 0;
-    ComplexNumber pCoNum1(1.22, 2.31);
+
+    ComplexNumber pCoNum1(1, 2);
     ComplexNumber pCoNum2(3.94, 2.09);
+
     sumReale = pCoNum1.parteReale + pCoNum2.parteReale;
     sumImmaginaria = pCoNum1.parteImmaginaria + pCoNum2.parteImmaginaria;
 
+    trasformazioneConiugato(pCoNum1.parteReale, pCoNum1.parteImmaginaria);
+
+    cout << "coniugato: " << pCoNum1.parteReale <<  pCoNum1.parteImmaginaria << "i"  << endl;
+
     cout << sumReale << endl;
     cout << sumImmaginaria << endl;
-    cout << "This is an example" << endl;
+
     return 0;
 }
